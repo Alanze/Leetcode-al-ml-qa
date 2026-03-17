5c2e2f808c5597b24da53edaf7b3839ab8646aed
Alanze <744356273@qq.com>
2026-03-17 13:55:52 +0800

    delete

--- test.sql
+++
@@ -1,9 +0,0 @@
-select user_id
-from (
-    select user_id,
-           date,
-           date - row_number() over(partition by user_id order by date) as flag
-    from login
-) t
-group by user_id, flag
-having count(*) >= 7;
